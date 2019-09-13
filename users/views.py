dc2e00842d47854ee0727a7f11db5f5ecb63aeee
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2019-09-13 18:21:00 +0530

    Add files via upload

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 def register(request):
 	if request.method == 'POST':
@@ -9,16 +10,37 @@ def register(request):
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
-			msg ='Account created for {}!'.format(username)
+			msg ='Your account is created ! You are now able to login'
 			messages.success(request, msg)
-			return redirect('blog-home')
+			return redirect('login')
 	else:
 		form = UserRegisterForm() 
 	return render(request, 'users/register.html', {'form': form})
 
-
+@login_required
+def profile(request):
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance = request.user)
+		p_form = ProfileUpdateForm(request.POST,
+								   request.FILES,
+								   instance = request.user.profile)
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			msg ='Your account has been successfully updated!!'
+			messages.success(request, msg)
+			return redirect ('profile')
+	else:
+		u_form = UserUpdateForm(instance = request.user)
+		p_form = ProfileUpdateForm(instance = request.user.profile)
+	context = {
+		'u_form': u_form,
+		'p_form': p_form
+	}
+	return render(request, 'users/profile.html', context)
 # message.debug
 # message.info
 # message.success
 # message.warning
 # message.error
+
